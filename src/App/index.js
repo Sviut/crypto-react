053916d36cd8264c9f7f97a703e9d2983b954ea2
Sviut <44402566+Sviut@users.noreply.github.com>
2020-03-29 12:48:56 +0300

    add AppBar grid

--- src/App/index.js
+++ src/App/index.js
@@ -1,14 +1,16 @@
 import React from 'react';
 import './App.css';
 
-import styled, {css} from 'styled-components'
+import styled, { css } from 'styled-components'
 
 import AppLayout from './AppLayout'
 import WelcomeMessage from './WelcomeMessage'
+import AppBar from './AppBar'
 
 function App() {
   return (
     <AppLayout>
+      <AppBar />
       <WelcomeMessage />
     </AppLayout>
   );
