f0fd6a25c2bc62caf6adf8ec1c6b95bb0115a685
Sviut <44402566+Sviut@users.noreply.github.com>
2020-03-30 14:43:18 +0300

    Updates

---
+++ precache-manifest.4d734ab0444438852139c55c612e8bc6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6f08fe4cd444a1052a75ebe10626fb9a",
+    "url": "/crypto-react/index.html"
+  },
+  {
+    "revision": "02a9278c84f41e60c02d",
+    "url": "/crypto-react/static/css/main.11df967e.chunk.css"
+  },
+  {
+    "revision": "66cd8ccfb4981b5b60bb",
+    "url": "/crypto-react/static/js/2.e3134c5e.chunk.js"
+  },
+  {
+    "revision": "64bea2e190f27355a76963b8cb94bb7b",
+    "url": "/crypto-react/static/js/2.e3134c5e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "02a9278c84f41e60c02d",
+    "url": "/crypto-react/static/js/main.3651c9f4.chunk.js"
+  },
+  {
+    "revision": "200d3de536461c282ee2",
+    "url": "/crypto-react/static/js/runtime-main.fd16c949.js"
+  }
+]);
