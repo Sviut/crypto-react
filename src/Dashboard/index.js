38cb8230a410eac6aa231bf947f776cc8b65029f
Sviut <44402566+Sviut@users.noreply.github.com>
2020-03-30 12:10:20 +0300

    add coin spotlight on dashboard

--- src/Dashboard/index.js
+++ src/Dashboard/index.js
@@ -1,9 +1,22 @@
 import React from 'react'
+import styled from 'styled-components'
+
 import Page from '../Shared/Page'
 import PriceGrid from './PriceGrid'
+import CoinSpotlight from '../Dashboard/CoinSpotlight'
+
+const ChartGrid = styled.div`
+  display: grid;
+  margin-top: 20px;
+  grid-gap: 25px;
+  grid-template-columns: 1fr 3fr;
+`
 
 export default function () {
   return <Page name="dashboard">
     <PriceGrid />
+    <ChartGrid>
+      <CoinSpotlight />
+    </ChartGrid>
   </Page>
 }
